296f3a867402e265d80417b66130ecc7b1622d08
alishamsa8 <ali.shamsaddinlou@gmail.com>
2025-07-29 17:56:48 +0200

    uv bug fixed

--- algorithm/mcp_servers/mcp_params.py
+++ algorithm/mcp_servers/mcp_params.py
@@ -5,5 +5,5 @@ load_dotenv(override=True)
 
 
 sql_mcp_server_params = [
-    {"command": "uv", "args": ["run", "algorithm/mcp_servers/mcp_sql_lineage/lineage_aql_server.py"]},
+    {"command": "python", "args": ["algorithm/mcp_servers/mcp_sql_lineage/lineage_aql_server.py"]},
 ]
